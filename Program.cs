fef76c976319d1298e800ab6b5effff1412ba889
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-01 23:58:48 -0300

    while

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+int numero = 0;
+
+while (numero < 150)
+{
+    // numero++ --> numero = numero + 1
+    numero +=10; // numero = numero + 10
+  WriteLine("O valor do numero é : " + numero);
+}
+WriteLine("Terminou...");     
